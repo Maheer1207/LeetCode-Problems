a77f9c685a6649719f625648a36370fd696e69a9
Maheer1207 <70773473+Maheer1207@users.noreply.github.com>
2022-11-16 21:53:57 -0700

    Working Solution of Longest Common Prefix

---
+++ Longest Common Prefix/Solution.cpp
@@ -0,0 +1,22 @@
+// Question: https://leetcode.com/problems/longest-common-prefix/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:string longestCommonPrefix(vector<string>& strs) {
+        int ans = strs[0].length(), n = strs.size();
+        for(int i=1; i<n; i++){
+            int j = 0;
+            while(j<strs[i].length() && strs[i][j]==strs[0][j])j++;
+            ans = min(ans, j);
+        }
+        return strs[0].substr(0, ans);
+    }
+};
+
+int main() {
+    Solution s;
+    vector<string> strs{"dog","racecar","car"};
+    cout << s.longestCommonPrefix(strs) << endl;
+}
